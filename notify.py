9f7a50d3773eb9219f117e336173245c7163baef
Aqeebmb <144835627+Aqeebmb@users.noreply.github.com>
2023-10-10 23:42:48 +0530

    notify.py

--- notify.py
+++ notify.py
@@ -1,21 +1,7 @@
-# import time
-# from plyer import notification
-
-# if __name__ == "__main__":
-#     while True:
-#         notification.notify(
-#             title = "ALERT!!!",
-#             message = "Take a break! It has been an hour!",
-#             timeout = 10
-#         )
-#         time.sleep(3600)
-
-
-
 import time
 from plyer import notification
 
-
+# setting condition
 if __name__ == "__main__":
     while True:
         notification.notify(
